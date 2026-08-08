05d7a160cf1fb6b68fc9aba14030429420951380
Turan Ismayilov <79365690+TuranIsmayilov@users.noreply.github.com>
2026-08-08 23:33:56 +0400

    Release 1.0.7: grab text out of a screenshot

--- shotpad/__init__.py
+++ shotpad/__init__.py
@@ -1,6 +1,6 @@
 """Shotpad - a cross-desktop screenshot annotation and beautifier."""
 
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 
 APP_ID = "io.github.TuranIsmayilov.Shotpad"
 APP_NAME = "Shotpad"
